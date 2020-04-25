8f35b181802df6182c87d69011be7e81f1a86bbc
dustryder <dust-ryder@hotmail.com>
2020-04-25 13:57:12 +1200

    Course template

---
+++ static/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+	var forms = document.querySelectorAll('.needs-validation');
+	forms.forEach(element => {
+		element.addEventListener('submit', (event) => {
+			if (element.checkValidity() === false) {
+				event.preventDefault();
+				event.stopPropagation();
+			}
+			element.classList.add('was-validated');
+		});
+	});
+});
